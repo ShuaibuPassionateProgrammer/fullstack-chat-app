b1bc8176fbc439757447055ee76bda92fbdec063
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-02-03 21:04:12 +0100

    First commit in the fullstack chat app

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,6 +18,14 @@ app.use(cors({
     credentials: true
 }));
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.use("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
+
 app.get("/", (req, res) => {
     res.send("Backend is ready...");
 });
