d7193b8a0c2a906f6d13a99252746f433e910357
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-02-04 14:23:58 +0100

    Initial commit in the fullstack chat app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,42 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import path from "path";
 
-const app = express();
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-    app.use("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-app.get("/", (req, res) => {
-    res.send("Backend is ready...");
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
-
-app.listen(port, () => console.log(`Server is up running on port: ${port}`));
