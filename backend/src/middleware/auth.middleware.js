651a24b1bd3d742ac1d0fd2a8a0cf9b76700632e
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-02-05 21:09:25 +0100

    Create auth middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,4 +1,12 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
-export const protectRoute = async (req, res, next) => {};
+export const protectRoute = async (req, res, next) => {
+    try {
+        //
+    }
+    catch (error) {
+    console.log("Error in protectRoute middleware: ", error?.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
