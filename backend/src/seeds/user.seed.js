497abb24d83d1091113c13860ae42688fcbfb2df
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-02-05 17:31:35 +0100

    Create user seed

--- backend/src/seeds/user.seed.js
+++ backend/src/seeds/user.seed.js
@@ -105,6 +105,7 @@ const seedDatabase = async () => {
         await connectDB();
 
         await User.insertMany(seedUsers);
+        console.log("Database seeded successfully!");
     }
     catch (error) {
         console.error("Error seeding database: " + error?.message);
