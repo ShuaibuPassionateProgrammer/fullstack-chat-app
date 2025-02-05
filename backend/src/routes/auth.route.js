65b38bb51ce2e67de4c27a0ca81fe5980022f0fe
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-02-05 17:18:19 +0100

    Create auth route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,4 +4,8 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
 export default router;
