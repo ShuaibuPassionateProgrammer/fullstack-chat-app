3c5326053846de8526e927d01cf8dedabc44adcb
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-02-05 21:09:59 +0100

    Create auth middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,7 +3,11 @@ import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
     try {
-        //
+        const token = req.cookies.jwt;
+
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized - No Token Provided" });
+        }
     }
     catch (error) {
     console.log("Error in protectRoute middleware: ", error?.message);
