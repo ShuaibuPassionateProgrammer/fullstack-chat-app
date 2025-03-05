2eec361ea6ff12991764e8b0165802db0b937b7e
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-03-05 15:29:55 +0100

    Working on Profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -3,6 +3,22 @@ import { useAuthStore } from "../store/useAuthStore";
 import { Camera, Mail, User } from "lucide-react";
 
 const ProfilePage = () => {
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = async () => {
+      const base64Image = reader.result;
+      setSelectedImg(base64Image);
+      await updateProfile({ profilePic: base64Image });
+    };
+  };
+  
   return (
     <div>
       <h1>Profile Page</h1>
