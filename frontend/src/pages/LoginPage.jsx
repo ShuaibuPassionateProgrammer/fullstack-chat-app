e3ca5438d4c7c2d078e44788cb895f6db4fb9a8b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-03-05 09:01:18 +0100

    Working on Login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import AuthImagePattern from "../components/AuthImagePattern";
+import { Link } from "react-router-dom";
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
 
 const LoginPage = () => {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const { login, isLoggingIn } = useAuthStore();
+
+    return (
+        <div>
+            <h1>Login Page</h1>
+        </div>
+    )
 }
 
 export default LoginPage
